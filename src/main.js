274d209330c811f2306fbfb6a6dec54f43054afb
eridenius <eridenius@gmail.com>
2025-02-21 08:49:13 +0100

    Added eventlistener and changed a class name

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const button = document.getElementById("my-button");
+
+button.addEventListener("click", doSomething);
+
+function doSomething() {
+  if ((document.body.style.backgroundColor = "white")) {
+    document.body.style.backgroundColor = "purple";
+  }
+
+  //document.body.style.backgroundColor = "purple";
+}
